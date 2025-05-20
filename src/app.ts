bdb3972761b48a8459329808b1674613313294a2
Aurélien <aurelien.strimelle@bstorm.be>
2025-05-20 13:43:35 +0200

    Exemple custom middleware 🥼

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import apiRouter from './routes';
 
 
@@ -6,7 +6,25 @@ import apiRouter from './routes';
 const app = express();
 
 //? Middleware
+//  - Custom - Temps de requete
+app.use((req: Request, res: Response, next: NextFunction) => {
+  
+  // Traitement avant les prochains middlewares
+  const timerStart = (new Date()).getTime();
+
+  // Passer au middleware suivant
+  next();
+
+  // Traitement après le traitements des middlewares
+  const timerEnd = (new Date()).getTime();
+  console.log(`(${req.method}) ${req.url} - ${timerEnd - timerStart}ms`);
+
+});
+
+//  - Gestion des données JSON
 app.use(express.json({ limit: '1mb' }));
+
+
 
 //! Routing
 app.use('/api', apiRouter);
