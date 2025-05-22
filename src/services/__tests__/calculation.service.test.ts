77eae61dbf1775bc96839f4ca332fe5567d6bd27
Aurélien <aurelien.strimelle@bstorm.be>
2025-05-22 09:43:42 +0200

    Testing - Apply differents TVA rates

--- src/services/__tests__/calculation.service.test.ts
+++ src/services/__tests__/calculation.service.test.ts
@@ -1,4 +1,4 @@
-import calculationService from '../calculation.service';
+import calculationService, { TVA } from '../calculation.service';
 
 describe("Calculation Service", () => {
 
@@ -24,6 +24,19 @@ describe("Calculation Service", () => {
 
     // Test
     const actual = calculationService.applyTVA(price);
+
+    // Assert
+    expect(actual).toBe(expected);
+  });
+
+  test.each<{ price: number, tva: TVA, expected: number; }>([
+    { price: 10.20, tva: 'NORMAL', expected: 12.342 },
+    { price: 10.20, tva: 'MIX', expected: 11.424 },
+    { price: 10.20, tva: 'FOOD', expected: 10.812 },
+  ])("Apply TVA rates $tva", ({ price, tva, expected }) => {
+
+    // Test
+    const actual = calculationService.applyTVA(price, tva);
 
     // Assert
     expect(actual).toBe(expected);
